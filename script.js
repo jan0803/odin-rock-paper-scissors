dff06a74c22dc4cfc70b9152d21c66e28e6a0064
jan0803 <126251323+jan0803@users.noreply.github.com>
2023-03-06 09:42:44 +0100

    Add eventlisteners for the buttons

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let playerScoreValue = 0;
 let computerScoreValue = 0;
+let rockButton = document.getElementById("rock");
+let paperButton = document.getElementById("paper");
+let scissorsButton = document.getElementById("scissors");
 
 function getComputerChoice() {
-    return Math.floor(Math.random()*3);
+    let computerChoiceNumber = Math.floor(Math.random()*3);
+    console.log(`Computer makes this choice: ${computerChoiceNumber}`);
+    return computerChoiceNumber;
 }
 
 // 0 = Rock
@@ -12,7 +17,7 @@ function getComputerChoice() {
 function playRound(playerSel, computerSel) {
 
     if (playerSel === computerSel) {
-        return "It's a draw!";
+        console.log("It's a draw!");
     }
     else if (
         ((playerSel === 0) && (computerSel === 2)) ||
@@ -21,27 +26,25 @@ function playRound(playerSel, computerSel) {
         ){
         playerScoreValue++;
         document.getElementById("player-score").innerHTML = playerScoreValue;
-        return "You win!";
+        console.log("Player scored");
     }
     else {
         computerScoreValue++;
         document.getElementById("computer-score").innerHTML = computerScoreValue;
-        return "You lose!";
+        console.log("Computer scored");
     }
 
 }
 
-
-function getPlayerChoice() {
-    return prompt();
-}
 
 function game() {
     playerScoreValue = 0;
     computerScoreValue = 0;
 
-    document.getElementById("rock").onclick = playRound(0,getComputerChoice());
-    document.getElementById("paper").onclick = playRound(1,getComputerChoice());
-    document.getElementById("scissors").onclick = playRound(2,getComputerChoice());
+    console.log("Initilization of the game");
+
+    rockButton.addEventListener('click', function () {playRound(0,getComputerChoice())});
+
 
 }
+
